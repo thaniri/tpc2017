a8bc2b9eef678e8e15c5fae661fe522719912f0a
Thaniri <thaniri@gmail.com>
2017-01-28 20:13:34 -0800

    updating database p.s. i just wasted an hour and a bit wondering why my queries werent working

--- sql/configure.php
+++ sql/configure.php
@@ -1,9 +1,10 @@
-<php?
+<?php
 
 $host = 'tpc2017.cnd231jurlnn.us-west-2.rds.amazonaws.com';
 $user = 'admintpc';
 $password = '3lmerfudd';
-$database = 'test';
+$database = 'main';
+
 //variables for establishing a connection to the database
 $link = mysqli_connect($host, $user, $password, $database);
 
