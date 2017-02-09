12107daed34c2c578c02e8f184f21e77dd268e5e
Thaniri <thaniri@gmail.com>
2017-02-09 14:24:24 -0800

    changed header look and dynamic header text

--- incs/header.php
+++ incs/header.php
@@ -1,13 +1,26 @@
 <?php
 
-echo'<header>
+$title = $_SERVER['PHP_SELF'];
+
+function page_title($url) {
+	$fp = file_get_contents($url);
+	if (!$fp) 
+		return null;
+
+	$res = preg_match("/<title>(.*)<\/title>/siU", $fp, $title_matches);
+	if (!$res) 
+		return null; 
+
+	// Clean up title: remove EOL's and excessive whitespace.
+	$title = preg_replace('/\s+/', ' ', $title_matches[1]);
+	$title = trim($title);
+	return $title;
+}
+
+//$title = page_title($_SERVER['PHP_SELF']);
 	
-	Header
-	
-	<div>
-		a b c d e f g h
-	</div>
-	
-</header>';
+echo'<header><h1>'
+. $title . 
+'</h1></header>';
 
 ?>
