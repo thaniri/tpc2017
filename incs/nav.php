dba37b4718f10f9f90a55e17772e4d62ec8fafd9
Thaniri <thaniri@gmail.com>
2017-02-09 14:46:20 -0800

    no more text decoration in css reset and logo redirects to home

--- incs/nav.php
+++ incs/nav.php
@@ -1,7 +1,7 @@
 <?php
 echo'<nav>
 	<div id="topNav">
-		<img src="./images/logo.svg" height="40px" width="40px"><span>TPC 2017</span></img>
+		<a href="./index.php"><img src="./images/logo.svg" height="40px" width="40px"><span>TPC 2017</span></img></a>
 	</div>
 	<div id="centerNav">
 		<ul>
