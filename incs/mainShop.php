7d4a771b848dc70577927693ba2fae10a1d33fad
Thaniri <thaniri@gmail.com>
2017-02-09 18:27:51 -0800

    added book covers to the shop page

--- incs/mainShop.php
+++ incs/mainShop.php
@@ -8,7 +8,7 @@
     $result = mysqli_query($link, 'select bTitle, bPrice, bQty, bCover, bGenre, bYear from book');
     if ($result->num_rows > 0) {
         while($row = $result->fetch_array()) {
-            echo '<div class="smallContent">Title: '. $row["bTitle"] . '<br/> Cover: ' . $row["bCover"] . '<br/> Price: ' . $row["bPrice"] . '<br/>Quantity: ' . $row["bQty"] . '<br/>Genre: ' . $row["bGenre"] . '<br/>Year: ' . $row["bYear"] . '</div>';
+            echo '<div class="smallContent">Title: '. $row["bTitle"] . '<br/> Cover: ' . '<img src="./images/bookCovers/'. $row['bCover'] . '"></img>' . '<br/> Price: ' . $row["bPrice"] . '<br/>Quantity: ' . $row["bQty"] . '<br/>Genre: ' . $row["bGenre"] . '<br/>Year: ' . $row["bYear"] . '</div>';
         }
     } 
     else {
