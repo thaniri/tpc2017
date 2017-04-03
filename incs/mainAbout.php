4b94a987b317ebcb72d2c64c24976f9eba897f73
Your Name <daniel.roberts1@email.kpu.ca>
2017-04-02 17:20:50 -0700

    about changes

--- incs/mainAbout.php
+++ incs/mainAbout.php
@@ -1,5 +1,26 @@
 <?php
 echo '<div class="largeContent">
-hi my name is daniel
+<p>What Languages we used:</p>
+<br/>
+<ul>HTML</ul>
+<ul>CSS</ul>
+<ul>PHP</ul>
+<ul>SQL</ul>
+<ul>JavaScript</ul>
+<br/>
+
+<p>How the Site is Run:</p>
+<br/>
+<p>The site is run using Xampp to host the PHP code locally without a server.
 </div>';
 ?>
+
+<!--
+
+- what languages we used
+- how the site is run (instructions for how to run it with Xampp)
+- the design of the code
+- database design
+- diagram of database (which is in documentation folder of Git)
+- mention git
+- anything else
